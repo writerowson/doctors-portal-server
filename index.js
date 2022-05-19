e5dddd6936c4b6d25adc389d810b31e1a9f6fe10
witerowson <runurowson@gmail.com>
2022-05-19 15:27:28 +0600

    upsert

--- index.js
+++ index.js
@@ -18,13 +18,15 @@ async function run() {
         await client.connect()
         const serviceCollection = client.db('dentist').collection('service')
         const bookingCollection = client.db('dentist').collection('booking')
+        const userCollection = client.db('dentist').collection('user')
 
 
         //  Api naming convention
-        // *app.get('/booking) get all bokking or get plural booking
+        // *app.get('/booking) get all booking or get plural booking
         // *app.get('/booking/:id) get a specific booking
         // *app.post('/booking) add a new booking
         // *app.delete ('/booking/:id) get a specific booking delete
+        // *app.put('/booking/:id)upsert =update (if exists)/insert(if doesn't)
         // *app.patch('/booking/:id) update booking
         app.get('/service', async (req, res) => {
             const query = {};
@@ -33,6 +35,53 @@ async function run() {
             res.send(services)
             console.log(7 > 6 && 8 < 6 && 6 > 4)
         });
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body
+            const filter = { email: email }
+            const options = { upsert: true }
+            const updateDoc = {
+                $set: user,
+            };
+
+            const result = await userCollection.updateOne(filter, updateDoc, options)
+            res.send(result)
+        })
+
+
+        app.get('/available', async (req, res) => {
+            const date = req.query.date || 'May 11, 2022';
+
+            // step 1 get all services
+            const services = await serviceCollection.find().toArray()
+            // step 2 get the booking of that day
+            const query = { date: date }
+            const bookings = await bookingCollection.find(query).toArray()
+            // step:3 for each service ;
+            services.forEach(service => {
+                // step 4 : find bookings for that service
+                const serviceBookings = bookings.filter(booking => booking.treatment === service.name)
+                // step 5 : select slots for the service Booking 
+                const bookedSlots = serviceBookings.map(book => book.slot)
+                // step 6 : select those slots that are not is bookedSlots
+
+                const available = service.slots.filter(slot => !bookedSlots.includes(slot));
+
+                service.slots = available
+                // service.booked = booked
+                // --------or------
+                // service.booked = serviceBookings.map(s => s.slot)
+            })
+            res.send(services)
+        })
+
+        app.get('/booking', async (req, res) => {
+            const patient = req.query.patient
+            const query = { patient: patient };
+            const bookings = await bookingCollection.find(query).toArray()
+            res.send(bookings)
+        })
 
         app.post('/booking', async (req, res) => {
             const booking = req.body;
